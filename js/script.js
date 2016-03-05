fc6932112c86f71d6618f8300cd143745ee9e4ee
PatrickSunico <apatricksunico@gmail.com>
2016-03-06 01:48:43 +0300

    "Initial Commit"

---
+++ js/script.js
@@ -0,0 +1,32 @@
+// sticky Nav Animation
+
+$(window).scroll(function() {
+
+    if($(this).scrollTop() > 1){
+        $('nav').addClass("sticky")
+    }
+    else{
+
+        $('nav').removeClass("sticky");
+    }
+});
+
+//jQuery to collapse the navbar on scroll
+$(window).scroll(function() {
+    if ($(".navbar").offset().top > 50) {
+        $(".navbar-fixed-top").addClass("top-nav-collapse");
+    } else {
+        $(".navbar-fixed-top").removeClass("top-nav-collapse");
+    }
+});
+
+//jQuery for page scrolling feature - requires jQuery Easing plugin
+$(function() {
+    $('a.page-scroll').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+});
